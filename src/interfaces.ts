8ebee6806a31253a95001e73f01cc1bfdc58285a
AlekseyOgorodnikov <alekseyogorodnikov@internet.ru>
2022-01-15 12:41:25 +0400

    interface

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -6,3 +6,51 @@ interface User {
 interface User {
   age: number;
 }
+
+let a: User = {
+  name: "Aleksey",
+  age: 32,
+};
+
+// interface Animal {
+//   eat(food: string): void;
+//   sleep(hours: number): void;
+// }
+
+// class Cat implements Animal {
+//   eat(food: string) {
+//     console.info("Ate some", food, ". Mmm!");
+//   }
+//   sleep(hours: number) {
+//     console.info("Slept for", hours, "hours");
+//   }
+// }
+
+interface Animal {
+  readonly name: string;
+  eat(food: string): void;
+  sleep(hours: number): void;
+}
+
+interface Animal {
+  readonly name: string;
+  eat(food: string): void;
+  sleep(hours: number): void;
+}
+
+interface Feline {
+  meow(): void;
+}
+
+class Cat implements Animal, Feline {
+  name = "Whiskers";
+  eat(food: string) {
+    console.info("Ate some", food, ". Mmm!");
+  }
+  sleep(hours: number) {
+    console.info("Slept for", hours, "hours");
+  }
+  meow() {
+    console.info("Meow");
+  }
+}
